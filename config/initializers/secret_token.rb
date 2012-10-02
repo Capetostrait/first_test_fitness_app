6d2e7d64dd5c3682baa519101832692fb96e73ad
Alex Asher <alex.asher@gmail.com>
2012-10-02 15:55:44 +1300

    Initialcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstTestFitnessApp::Application.config.secret_token = '0ba7d6d2482651f464aaaf35bf89325ec83b7f7470db4bb2ec3a79030eb31eef6fb6bd999b4a233fee6782976eb81c229a5da27ad64edcc5d3acef1da21b1d45'
